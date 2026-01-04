c0943d08449835218a357814a6d027a5bdfb20fb
pbertolani <pedrobertolani22@gmail.com>
2026-01-03 22:09:58 -0300

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-torrar("pão de forma", "Pedro", 10.90)
+soma(5, 5)
 
-function torrar(pao, nome = "Cliente", valor){
-    console.log("Torrada feita com " + pao);   
-    console.log("Ela é um pedido de " + nome);
-    console.log("O valor total é " + valor)
-}
+function soma(numA, numB){
+    let somatorio =numA + numB
+    console.log(somatorio);
+};

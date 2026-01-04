524d4ee7c4d77ad9eb81cd9546dcf818585196cd
pbertolani <pedrobertolani22@gmail.com>
2026-01-03 21:54:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+torrar("pão de forma", "Pedro", 10.90)
+
+function torrar(pao, nome = "Cliente", valor){
+    console.log("Torrada feita com " + pao);   
+    console.log("Ela é um pedido de " + nome);
+    console.log("O valor total é " + valor)
+}

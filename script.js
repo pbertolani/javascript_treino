240d7a2f975213cd0ff12d22e55d8fa6d52020e2
pbertolani <pedrobertolani22@gmail.com>
2026-01-03 22:15:47 -0300

    forth commit

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let resultado = soma(5, 5)
 console.log("O resultado dessa função é " + resultado)
 
 function soma(numA, numB){
-    let somatorio =numA + numB
+    let somatorio = numA + numB
     return(somatorio)
 };
